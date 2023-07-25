3d532011bf584776ddb0d0754aeed18998e42302
content-cn <87414959+content-cn@users.noreply.github.com>
2023-07-25 12:45:31 +0530

    Add files via upload

---
+++ models/stocksschema.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+
+// Define the data schema for each time range (5y, 1y, 1mo, 3mo)
+const DataSchema = new mongoose.Schema({
+  value: [Number],
+  timeStamp: [Number],
+});
+
+// Define the schema for each stock (MSFT, GOOGL, AMZN)
+const StockSchema = new mongoose.Schema({
+  '5y': DataSchema,
+  '1y': DataSchema,
+  '1mo': DataSchema,
+  '3mo': DataSchema,
+});
+
+// Create the model for the entire data structure
+const StockDataSchema = new mongoose.Schema({
+  MSFT: StockSchema,
+  GOOGL: StockSchema,
+  AMZN: StockSchema,
+});
+
+const Stocks = mongoose.model("Stocks", StockDataSchema);
+
+export default Stocks;
